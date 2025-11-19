2d7fd89792a9af904d4c465030a3ad6580109df3
batzionL <batziona99@gmail.com>
2025-11-19 12:14:58 +0200

    double check

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@{}/{}'.format(
     os.getenv('DB_HOST', 'mysql'),
     os.getenv('DB_NAME', 'dbtodo')
 )
-#add commet to check the work-flow
+
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
